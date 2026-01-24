4e17eb825d0df5edcf72ad960de8c3e9043ffcc2
ghostnerd <gitlost101@gmail.com>
2026-01-24 16:59:36 +0530

    This is a python learning file

---
+++ hello.py
@@ -0,0 +1,120 @@
+print ("Hello world\n")
+greeting = "Welcome to the new repo!"
+print (greeting)
+for i in range(5):
+    print (greeting)
+
+import sys
+print("Python version")
+print (sys.version)
+
+if(5 > 5):
+      for i in range(3):
+        print("5 is greater than 2")
+print("Five is greater than two!")
+
+#this is a commet, and should not be executed
+#this is another comment
+#this is yet another comment
+#this is the last comment
+#this is the final comment
+#this is the ultimate comment
+#this is the penultimate comment
+#this is the antepenultimate comment
+#this is the preantepenultimate comment
+#this is the superlative comment
+#this is the definitive comment
+#this is the conclusive comment
+#this is the terminal comment
+#this is the closing comment
+#this is the ending comment
+#this is the finishing comment
+#this is the concluding comment
+#this is the wrap-up comment
+#this is the finalizing comment
+#this is the last but not least comment
+#this is the ultimate final comment
+#this is the absolute final comment
+#this is the conclusive final comment
+#this is the definitive final comment
+
+print("Just a test line to check git functionality") #print("Another test line to check nextline functionality")
+
+#this is to test sameline different print statements using "End" parameter
+print("This is the first part of the line ", end="" )
+print("and this is the second part of the same line.")
+print("This is to test the end parameter again ", end="*** ")
+print("See if it works this time.")
+print("End of the test lines.")
+
+print(23+32)
+print("I am", 35, "years old.")
+#this is to play with varaibles
+x = 4       # x is of type int
+x = "Sally" # x is now of type 
+x=12 # x is of type int again
+print(x)
+
+#this is casting example
+
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0
+
+print(x)
+print(y)
+print(z)
+
+#this is ot check type of above casted variables
+print("the type of x is", type(x))
+print("the type of y is", type(y))
+print("the type of z is", type(z))
+
+#variable names case sensitivity checkup
+a = 4
+A = "Sally" # A is a different variable than a
+print(a)
+print(A)
+    #variable names are case sensitive in python
+    ########################################
+    #########################################
+#this is valid variable naming section
+myvar="John"
+my_var="John"
+_my_var="John"
+myVar="John"
+MYVAR="John"
+myvar2="John"
+
+#this is a invalid variable naming section
+#2myvar="John"  #invalid variable name there is a number at the beginning
+#my-var="John"  #invalid variable name there is a hyphen
+#my var="John"  #invalid variable name there is a space
+#print(my var)
+
+#this is cases to write multi word variable names
+#this is one case
+MyVariableName="John" #pascal case -- Every word starts with a capital letter
+#this is second case
+my_variable_name="John" #snake case -- Every word is in lowercase and separated by underscores
+#this is third case
+myVariableName="John" #camel case -- first letter of first word is in lowercase and rest of the words start with a capital letter
+
+#this is to check assignment of multiple variables in one line
+
+Name, Residence, Age ="Ammaar", "Kashmir", 23
+print(Name)
+print(Residence)
+print(Age)
+
+####################################################################################
+####################################################################################
+
+#this is to check the assignment of same value to multiple variables in one line
+x = y = z = "Orange"
+print(x)
+print(y)
+print(z)
+
+####################################################################################
+####################################################################################
